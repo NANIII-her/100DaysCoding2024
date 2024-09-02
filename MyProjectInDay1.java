5a95fc7e672537f5584c7e7f046df5cf804c1c4b
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-02 12:28:27 +0800

    Update and rename MyProject.java to MyProjectInDay1.java

--- MyProjectInDay1.java
+++ MyProjectInDay1.java
@@ -1,6 +1,6 @@
 package Day1;
 
-public class MyProject{
+public class MyProjectInDay1{
     public static void main(String[] args){
         //Di abwah ini adalah main methodnya
         System.out.println("=======================================");
