0fca3d3a2b088b43d36f83f49526a93618c3b58b
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-02 12:25:45 +0800

    Hari 1
    Menampilkan bidata saya dengan menggunakan println, print tapi menggunakan \n dan menggunakan printf. Saya menggunakan integer untuk bisa menggunakan printf, apakah tidak apa-apa?
    
    Sekian...

---
+++ MyProject.java
@@ -0,0 +1,24 @@
+package Day1;
+
+public class MyProject{
+    public static void main(String[] args){
+        //Di abwah ini adalah main methodnya
+        System.out.println("=======================================");
+
+        System.out.println("Nama saya adalah Khoiri Nur Umar");
+        System.out.print("Saya dari kelas Informatika C \n");
+        System.out.print("Ini adalah program pertama saya \n");
+
+        System.out.println("=======================================");
+        
+        //Berikut adalah programnya
+        int a = 10;
+        int b = 15;
+
+        int hasil = a + b;
+        System.out.printf("%d + %d = %d \n", a, b, hasil);
+
+        /*SEKIAN DARI SAYA
+          MOHON MAAF JIKA ADA KESALAHAN */
+    }
+}
