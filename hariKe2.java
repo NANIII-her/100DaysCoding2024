33bab72d30bd877740042667ab696a1c5a0d5f4a
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-03 10:13:27 +0800

    Proyek di hari kedua
    Membuat 2 buah variabel dengan menggunakan tanda "+" sebagai pemisah antara nilai variabel dengan string. Maaf kalau salah kak...

---
+++ hariKe2.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class HariKe2 {
+    public static void main(String[] args){
+        System.out.println("Ini adalah proyek saya di hari kedua!");
+
+        int a = 4;
+        int b = 4;
+        int hasil = a * b; 
+
+        System.out.println(a + " x " + b + " = " + hasil);
+    }
+}
