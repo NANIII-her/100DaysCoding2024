00fb8d2e49f793f29749501337cfd7ffb0526d0e
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-04 20:17:07 +0800

    Update HariKe3.java
    Oke kak

--- HariKe3.java
+++ HariKe3.java
@@ -1,18 +1,11 @@
-package Day3;
-
-public class Day3 {
+public class HariKe3{
     public static void main(String[] args){
-        System.out.println("Katanya disuruh untuk mengkreasikan sout");
-        System.out.println("Baiklah kalau begitu");
-
-        System.out.println("========================================================================================================================================");
+        System.out.println("Ini codingan ku kak");
         
+        int x = 15;
+        int y = 7;
+        float hasilBagi = (float)x / y;
         
-        System.out.println("|\\     |   /----\\   |\\     | -------  -------  ---------");
-        System.out.println("| \\    |  |      |  | \\    |    |        |         |");
-        System.out.println("|  \\   |  |      |  |  \\   |    |        |         |");
-        System.out.println("|   \\  |  |------|  |   \\  |    |        |         |");
-        System.out.println("|    \\ |  |      |  |    \\ |    |        |         |");
-        System.out.println("|     \\|  |      |  |     \\| ___|___  ___|___  ____|____");
-    }    
+        System.out.printf("%d / %d = %f \n", x, y, hasilBagi);
+    }
 }
