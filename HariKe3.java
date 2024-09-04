0228862e8ce5e4a5b8886dde416bdd5340a6ee31
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-04 18:32:47 +0800

    Hari ke-3
    Sedikit kreasi dan membentuk nickname saya yaitu "NANIII".
    
    Maaf kalau ada salah kak

---
+++ HariKe3.java
@@ -0,0 +1,18 @@
+package Day3;
+
+public class Day3 {
+    public static void main(String[] args){
+        System.out.println("Katanya disuruh untuk mengkreasikan sout");
+        System.out.println("Baiklah kalau begitu");
+
+        System.out.println("========================================================================================================================================");
+        
+        
+        System.out.println("|\\     |   /----\\   |\\     | -------  -------  ---------");
+        System.out.println("| \\    |  |      |  | \\    |    |        |         |");
+        System.out.println("|  \\   |  |      |  |  \\   |    |        |         |");
+        System.out.println("|   \\  |  |------|  |   \\  |    |        |         |");
+        System.out.println("|    \\ |  |      |  |    \\ |    |        |         |");
+        System.out.println("|     \\|  |      |  |     \\| ___|___  ___|___  ____|____");
+    }    
+}
