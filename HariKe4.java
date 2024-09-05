b79b8ad1f554c772d999746f6efa6d5a9c834ad9
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-05 19:10:55 +0800

    Hari ke-4

---
+++ HariKe4.java
@@ -0,0 +1,31 @@
+package SeratusHariNgoding;
+
+public class HariKe4{
+    public static void main(String[] args){
+        System.out.print("Ini adalah program saya di hari ke-4 \n");
+        
+        //Terlebih dahulu saya mendeklarasikan variabel x, y dan z sebagai integer
+        
+        String nama, alamat, prodi, hobi;
+        
+        nama = "Khoiri Nur Umar. B";
+        alamat = "Baruga";
+        prodi = "Informatika C";
+        hobi = "Bermain game dan bermain voli";
+        
+        System.out.printf("Nama   : %s \n", nama);
+        System.out.printf("Alamat : %s \n", alamat);
+        System.out.printf("Prodi  : %s \n", prodi);
+        System.out.printf("Hobi   : %s \n", hobi);
+        
+        System.out.println("================================================");
+        
+        //Selain itu, saya mencoba menggunakan "printf" tanpa memerlukan variabel
+        System.out.printf("Berat badan  : %f \n", 69.5);
+        System.out.printf("Tinggi badan : %f \n", 165.2);
+        
+        
+        /*Ternyata bisa ji pale' tanpa variabel
+          Tapi di belakang koma, harus  ki' mengisi nilainya untuk "%f"   */
+    }
+}
