f92d1fc27c86ff265b9431d03304b2780b9fe500
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-06 14:44:37 +0800

    Hari ke-5

---
+++ HariKe5.java
@@ -0,0 +1,13 @@
+public class HariKe5{
+    public static void main(String[] args){
+        System.out.println("Ini adalah hari ke-5");
+
+        //Saya akan membuat yang sederhana saja (single line)
+        System.out.println("Sudah selesai");
+
+        /* Sudah
+         * kak
+         * Selesai mi (multi line)
+         */
+    }
+}
