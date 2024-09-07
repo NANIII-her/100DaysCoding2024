9f9d805a9f98646904ddf2491fede99289dd40b1
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-07 09:08:26 +0800

    Hari ke-6
    Variabel: Deklarasi

---
+++ HariKe6.java
@@ -0,0 +1,15 @@
+public class HariKe6 {
+    public static void main(String[] args){
+        System.out.println("Ini adalah proyek hari ke-6 saya");
+
+        //Membuat sebuah variabel deklarasi
+        byte x, y, z, hasil;
+        x = 7;
+        y = 4;
+        z = 9;
+
+        hasil = (byte) (x * (z - y));
+
+        System.out.printf("%d (%d - %d) = %d", x, y, z, hasil);
+    }
+}
