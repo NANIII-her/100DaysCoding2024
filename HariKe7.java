314f119f09f9d0780a9caec4fccd078cef1e91d5
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-08 19:24:34 +0800

    Hari ke-7
    Inisialisasi

---
+++ HariKe7.java
@@ -0,0 +1,16 @@
+public class HariKe7 {
+    public static void main(String[] args){
+        System.out.println("Ini adalah proyek hari ke-7 saya");
+
+        // Di bawah ini adalah inisialisasi
+        int a = 10;
+        byte b = 15; 
+        System.out.println(a + " dan " + b);
+
+        float x = 10.1f;
+        float y = 15.1f;
+        System.out.println(x + " dan " + y);
+
+        System.out.println("------------Sekian dan terima kasih---------------");
+    }
+}
