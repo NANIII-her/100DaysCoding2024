64d6933d036bd14ec604869c34a30b00112bd133
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-09 12:13:08 +0800

    Hari ke-8
    Variabel update

---
+++ HariKe8.java
@@ -0,0 +1,16 @@
+public class HariKe8 {
+    public static void main(String[] args){
+        System.out.println("Ini adalah haro ke-8");
+
+        int a, b;
+        a = 100;
+        a = 200; //Ini adalah perubahan nilai variabel (update)
+
+        b = 1000;
+        b = 2000;
+
+        System.out.println("Perubahan nilai variabel a = " + a);
+
+        System.out.println("Perubahan nilai variabel b = " + b);
+    }
+}
