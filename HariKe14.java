1b5476dfdd0e0b78b5901e92960d948f9d4f0be7
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-16 05:46:50 +0800

    Hari ke-14
    Menggunakan tipe data long.
    
    Maaf saya terlambat karena banyak tugas hahahaha

---
+++ HariKe14.java
@@ -0,0 +1,20 @@
+public class HarKe14 {
+    public static void main(String[] args) {
+        
+        System.out.println("Maaf saya terlambat");
+        System.out.println("Ini adalah proyek hari ke-14");
+
+        //Tipe data long
+        long wawa, heri, flora, juhariana, fauziah, hasil;
+
+        wawa = 1000;
+        heri = 10000; 
+        flora = 100000;
+        juhariana = 1000000;
+        fauziah = 10000000;
+
+        hasil = (juhariana * wawa) / fauziah + flora - heri;
+
+        System.out.println("Hasilnya adalah " + hasil); //Hasilnya 90.100
+    }
+}
