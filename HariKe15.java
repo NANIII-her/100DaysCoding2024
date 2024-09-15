a94dc1f21caab4ddea289531bfa804a280f4cd44
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-16 05:53:48 +0800

    Hari ke-15
    Menggunakan tipe data double sebagai hasil pembagian.

---
+++ HariKe15.java
@@ -0,0 +1,16 @@
+public class HariKe15 {
+    public static void main(String[] args){
+
+        System.out.println("Ini adalah proyek hari ke-15");
+
+        //Tipe data double
+        int a, b;
+        a = 7;
+        b = 4;
+
+        //Mengunakan tipe data double sebagai hasil pembagian, nanti hasilnya akan berbentuk bilangan koma 
+        double hasil = (double) a / b;  
+
+        System.out.println("Hasilnya = " + hasil);
+    }
+}
