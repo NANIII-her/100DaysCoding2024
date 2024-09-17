e7b73b56f0fb6a25d7827faf7f22a7bc42980b03
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-17 12:54:24 +0800

    Hari ke-16
    Menggunakan tipe data float

---
+++ HariKe16.java
@@ -0,0 +1,18 @@
+public class HariKe16 {
+    public static void main(String[] args) {
+        
+        System.out.println("Ini adalah proyek hari ke-16");
+
+        //Tipe data float
+        float x, y, z;
+        x = 20.12f;
+        y = 12.34f;
+        z = 29.76f;
+
+        float hasil = (x + y) - z;
+
+        System.out.println("Hasilnya adalah " + hasil);
+
+        
+    }
+}
