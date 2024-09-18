6ef5b3aef51ac72e866277e431457a73ed3505db
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-18 19:04:32 +0800

    Hari ke-17
    Menggunakan tipe data char (character) untuk mencetak huruf du setiap huruf nama panjang saya.

---
+++ HariKe17.java
@@ -0,0 +1,22 @@
+public class HariKe17 {
+    public static void main(String[] args) {
+        
+        System.out.println("Ini adalah proyek hari ke-17");
+
+        char a = 'K';
+        char b = 'h';
+        char c = 'o';
+        char d = 'i';
+        char e = 'r';
+        char f = 'i';
+        char g = 'N';
+        char h = 'u';
+        char i = 'r';
+        char j = 'U';
+        char k = 'm';
+        char l = 'a';
+        char m = 'r';
+
+        System.out.println("" + a + b + c + d + e + f + " " + g + h + i + " " + j + k + l + m);
+    }
+}
