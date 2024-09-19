03dd44b41b9759d099c6600aeebe09474247d4ca
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-19 23:28:57 +0800

    Hari ke-18
    Penggunaan tipe data boolean

---
+++ HariKe18.java
@@ -0,0 +1,16 @@
+public class HariKe18 {
+    public static void main(String[] args) {
+        
+        System.out.println("Ini adalah proyek hari ke-18");
+
+        //TIpe data boolean
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("Nilai boolean a = " + a);
+        System.out.println("Nilai boolean b = " + b);
+
+        //Sigkat karena dikejar waktu
+
+    }
+}
