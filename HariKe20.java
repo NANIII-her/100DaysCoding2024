0156f1490ead7968969d5b8c7fde247d0f441835
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-21 21:09:31 +0800

    Hari ke-20
    Penggunaan tipe data reference Byte

---
+++ HariKe20.java
@@ -0,0 +1,14 @@
+public class HariKe20 {
+    public static void main(String[] args) {
+        
+        System.out.println("Ini adalah proyek saya di hari ke-20");
+
+        Byte a,b,hasil;
+        a = 23;
+        b = 12;
+
+        hasil = (byte) (a + b);
+
+        System.out.println("Hasil penjumlahan = " + hasil);
+    }
+}
