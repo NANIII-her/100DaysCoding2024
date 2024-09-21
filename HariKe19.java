1f5d7ed0bdc0e876f411f168aa39d12977fc968c
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-21 18:40:12 +0800

    Hari ke-19
    Menggunakan tipe data String

---
+++ HariKe19.java
@@ -0,0 +1,17 @@
+public class HariKe19 {
+    public static void main(String[] args) {
+        
+        System.out.println("Ini adalah proyek saya di hari ke-19");
+
+        String namaLengkap, namaPanggilan, alamat;
+
+        namaLengkap = "Khoiri Nur Umar";
+        namaPanggilan = "Heri";
+        alamat = "Baruga";
+
+        System.out.println("Nama saya adalah " + namaLengkap);
+        System.out.println("Biasa dipanggil " + namaPanggilan);
+        System.out.println("Saya tinggal di " + alamat);
+
+    }
+}
