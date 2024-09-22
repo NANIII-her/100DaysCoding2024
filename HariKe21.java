4628a7426674048d69723aa21a1bb24e377981bc
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-22 21:15:40 +0800

    Hari ke-21
    Tipe data reference Short

---
+++ HariKe21.java
@@ -0,0 +1,19 @@
+public class HariKe21 {
+    public static void main(String[] args) {
+        
+        System.out.println("Ini adalah proyek saya di hari ke-21");
+
+        //Tipe data reference short
+        Short a,b;
+        a = 30000;
+        b = 2000;
+
+        System.out.println("Uangku = Rp." + a);
+        System.out.println("Uangnya Wawa = Rp." + b);
+
+        System.out.println("Cepat habis uangnya karena boros ii kak");
+        System.out.println("Ang ang ang ang");
+
+        //Just kidding Wawa
+    }
+}
