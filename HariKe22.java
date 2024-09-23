a5abcf98cede202eb0054b72e288846c2a8b6dbe
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-23 21:32:47 +0800

    Hari ke-22
    Tipe data reference Integer

---
+++ HariKe22.java
@@ -0,0 +1,17 @@
+public class HariKe22 {
+    public static void main(String[] args) {
+        
+        System.out.println("Ini adalah proyek saya di hari ke-22");
+
+        //Tipe data reference integer
+        Integer a,b,hasil;
+        a = 4;
+        b = 6;
+
+        hasil = a + b;
+
+        System.out.println(hasil);
+
+
+    }
+}
