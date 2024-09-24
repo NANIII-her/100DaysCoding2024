365105185c1a5ed6b4ed9c4b1993373ddee7e6cc
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-24 07:37:11 -0700

    Hari ke-23
    Menggunakan tipe data reference Long

---
+++ HariKe23.java
@@ -0,0 +1,19 @@
+package pkg100haringoding;
+
+public class HariKe23 { 
+    public static void main(String[] args) {
+        
+        System.out.println("Ini adalah proyek saya di hari ke-23");
+
+        //tipe data reference Long
+        Long a,b,c,hasil;
+        a = 12L;
+        b = 21L;
+        c = b;
+        b = a;
+        
+        hasil = c + b - a;
+        System.out.println("Hasilnya = " + hasil);
+
+    }
+}
