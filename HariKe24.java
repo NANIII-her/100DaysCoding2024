fe90e60262b67ccfdecb7998279150c222163af7
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-25 23:49:22 +0800

    Hari ke-24
    Penggunaan tipe data reference Float

---
+++ HariKe24.java
@@ -0,0 +1,15 @@
+public class HariKe24 {
+    public static void main(String[] args) {
+        
+        System.out.println("Ini adalah proyek saya di hari ke-24");
+
+        Float a,b,c;
+
+        a = 9000.3f;
+        b = 3000.3f;
+        c = 4000.3f;
+
+        System.out.println("Hasil perkalian = " + (a * b * c));
+    }
+
+}
