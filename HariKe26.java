7d2cfcdc49dd3f3886741a3c917889bf491d7eda
NANIII-her <rahmakhumairah3307@gmail.com>
2024-09-27 21:35:04 +0800

    Hari ke-26

---
+++ HariKe26.java
@@ -0,0 +1,14 @@
+public class HariKe26 {
+
+public static void main(String... Okay){
+
+Character w,n,i;
+w = 'W';
+n = 'N';
+i = 'I';
+
+System.out.println("Aku adalah " + w + n + i);
+System out.println("Nasionalisme ygy");
+
+}
+}
