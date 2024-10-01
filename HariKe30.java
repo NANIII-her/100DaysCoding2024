e4945c66253bc1ec622c51658dc2d74c8ff92d3c
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-01 20:37:00 +0800

    Hari ke-30

---
+++ HariKe30.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+
+public class anjay{
+    public static void main(String[] args){
+        
+        System.out.println("oke lah");
+        
+        BigDecimal mm = new BigDecimal ("1.000000000"); 
+        
+        System.out.println("Uangnya Wawa: Rp." + mm);
+    }
+}
