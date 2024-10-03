bf38c411f8b6f4204b60379e93f8ff0f0751d8c1
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-03 21:33:47 +0800

    Hari ke-32

---
+++ HariKe32.java
@@ -0,0 +1,36 @@
+public class HariKe32 {
+    public static void main(String[] args) {
+        
+        byte njay = 72;
+        String a = Byte.toString(njay);
+        System.out.println("Byte = " + a);
+
+        short anjay = 29385;
+        String b = Short.toString(anjay);
+        System.out.println("Short = " + b);
+
+        int bjir = 3945914;
+        String c = Integer.toString(bjir);
+        System.out.println("Integer = " + c);
+
+        long njir = 293859450;
+        String d = Long.toString(njir);
+        System.out.println("Long = " + d);
+
+        double jir = 92735.10;
+        String e = Double.toString(jir);
+        System.out.println("Double = " + e);
+
+        float cihuyy = 2588945.39437f;
+        String f = Float.toString(cihuyy);
+        System.out.println("Float = " + f);
+
+        boolean woilah = true;
+        String g = Boolean.toString(woilah);
+        System.out.println("Boolean = " + g);
+
+        char hehehe = 'N';
+        String h = Character.toString(hehehe);
+        System.out.println("Character = " + h);
+    }
+}
