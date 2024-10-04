eddfe3316d085f444c7fbbc4b0b59c8561956f73
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-04 21:24:01 +0800

    Hari ke-33
    Casting

---
+++ HariKe33.java
@@ -0,0 +1,20 @@
+public class HariKe33{
+    public static void main(String[] args) {
+        
+        System.out.println("Okelah");
+
+        //Pemaksaan hahaha
+        byte wahahaha = (byte) 128;
+        System.out.println(wahahaha);
+
+        byte a,b;
+        a = 50;
+        b = 10;
+
+        byte hasil = (byte) (a + b);
+        System.out.println(hasil);
+
+        int oke = Integer.valueOf(wahahaha);
+        System.out.println(oke);
+    }
+}
