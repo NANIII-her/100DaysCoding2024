41622c15dc525042efd621592e36009f2e3930f0
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-06 23:29:03 +0800

    Hari ke-35

---
+++ HariKe35.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class HariKe35{
+    public static void main(String[] args) {
+        
+        int a,b;
+        a = 12;
+        b = 12;
+
+        System.out.println(a + b);
+
+        a += b;
+        int c = a + b;
+        System.out.println(a + c);
+        
+    }
+}
