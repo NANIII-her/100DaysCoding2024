5aaa4238d873f63387ff25366e0eea0bda88db8f
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-07 21:12:25 +0800

    Hari ke-36

---
+++ HariKe36.java
@@ -0,0 +1,12 @@
+public class HariKe36{
+    public static void main(String[] args) {
+        
+        //Mode dikejar deadline
+        System.out.println("30 x 2 = " + (30 * 2)); // Perkalian
+        System.out.println("30 / 6 = " + (30 / 6)); // Pembagian
+        System.out.println("30 % 4 = " + (30 % 4)); // Modulus
+
+        //Maaf kak, ini dibuat sangat singkat kerena dikejar tugas yang menumpuk
+        
+    }
+}
