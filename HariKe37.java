28d932d0bbc1e776df889c0751883773efa64024
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-08 23:17:30 +0800

    Hari ke-37

---
+++ HariKe37.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HariKe37{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("A = ");
+        int a = sc.nextInt();
+
+        System.out.print("B = ");
+        int b = sc.nextInt();
+
+        System.out.println("Hasil jumlahnya adalah " + (a + b));
+        System.out.println("Hasil kurangnya adalah " + (a - b));
+        
+    }
+}
