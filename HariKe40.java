084d49cb292277a81358050e934147c71e575e72
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-11 20:35:50 +0800

    Hari ke-40

---
+++ HariKe40.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class HariKe40{
+    public static void main(String[] args) {
+        
+        int a,b,c,d,e,f,g;
+        a = 10;
+        b = 2;
+        c = 9;
+        d = c;
+        e = a;
+        f = b;
+        g = 7;
+        b += e;
+        b = 6;
+        a = g;
+        g -= d;
+
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(g > f); 
+        System.out.println(d < g); 
+        System.out.println(e < 2); 
+        System.out.println(f > 15); 
+        System.out.println(a < e); 
+        System.out.println(f > b); 
+        System.out.println(b);
+        
+    }
+}
