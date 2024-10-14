7615c6a890fda36059ec01075ac27f49ffd444ad
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-14 19:30:13 +0800

    Hari ke-43

---
+++ HariKe43.java
@@ -0,0 +1,14 @@
+public class HariKe43 {
+    public static void main(String[] args) {
+        
+        int a,b,c;
+        a = 15;
+        b = 13;
+        c = 20;
+
+        System.out.println(a >= a);
+        System.out.println(a >= b);
+        System.out.println(a <= c);
+    }
+    
+}
