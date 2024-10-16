9c39ad18c8c2d858bbe212eacfb90abbd811bc0e
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-16 20:09:17 +0800

    Hari ke-44

---
+++ HariKe44.java
@@ -0,0 +1,18 @@
+public class HariKe44 {
+    public static void main(String[] args) {
+
+        boolean a,b,c;
+
+        a = true;
+        b = false;
+        c = true;
+
+        boolean d = a && b;
+
+        System.out.println(d && b);
+        System.out.println(a && c);
+        System.out.println(c && d);
+
+        
+    }
+}
