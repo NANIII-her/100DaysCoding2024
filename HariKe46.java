358a6c2a555b6f2332840a352e78da438bd63e9e
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-17 19:23:45 +0800

    Hari ke-46

---
+++ HariKe46.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HariKe46 {
+    public static void main(String[] args) {
+
+        //Logika not
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai: ");
+        boolean a = sc.nextBoolean();
+
+        boolean hasil = !a;
+
+        System.out.println("Hasil not nya adalah " + hasil);
+
+        
+    }
+}
