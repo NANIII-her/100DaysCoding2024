484dbaeee71e62d0a5911b74fc327ce361b1e889
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-24 20:21:15 +0800

    Hari ke-53

---
+++ HariKe53.java
@@ -0,0 +1,10 @@
+public class HariKe53 {
+    public static void main(String[] args) {
+        for(;;){
+            System.out.println("AKU CINTA INDONESIA!!!!!");
+        } //Ini versi singkatnya
+
+        //agak males juga sih sebenarnya
+
+    }
+}
