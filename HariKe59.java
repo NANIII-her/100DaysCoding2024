7dcc1d1f940341fec59804b5a1ddc00a90d77a83
NANIII-her <rahmakhumairah3307@gmail.com>
2024-10-30 22:39:20 +0800

    Hari ke-59

---
+++ HariKe59.java
@@ -0,0 +1,11 @@
+public class HariKe59 {
+    public static void main(String[] args) {
+
+        char a[] = {'K', 'H', 'O', 'I', 'R', 'I'};
+
+        for (char c : a) {
+            System.out.print(c);
+        }
+        
+    }
+} 
