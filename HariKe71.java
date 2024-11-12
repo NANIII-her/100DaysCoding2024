21a46a6d7270d63af2bca9c5a337e9c1c784b7e7
NANIII-her <rahmakhumairah3307@gmail.com>
2024-11-12 21:17:38 +0800

    Hari ke-71

---
+++ HariKe71.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HariKe71 {
+    public static void heri() {
+        int a = 12;
+        int b = 12;
+        int hasil = a + b;
+        System.out.println("Hasilnya adalah " + hasil);
+    }
+
+    public static void main(String[] args) {
+        HariKe71.heri();
+    }
+}
