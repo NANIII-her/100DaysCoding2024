ee46e60e7bdc5557d9b06ee6a7739da1c8cfd2f9
NANIII-her <rahmakhumairah3307@gmail.com>
2024-11-19 19:05:05 +0800

    Hari ke-79

---
+++ HariKe79.java
@@ -0,0 +1,21 @@
+public class HariKe79 {
+    public static void main(String[] args) {
+
+        int a = 18;
+        float b = 18.9f;
+        boolean c = true;
+        char d = 'H';
+
+        String a1 = Integer.toString(a);
+        String b1 = Float.toString(b);
+        String c1 = Boolean.toString(c);
+        String d1 = Character.toString(d);
+
+        System.out.println(a1);
+        System.out.println(b1);
+        System.out.println(c1);
+        System.out.println(d1);
+
+        // Sangat gampang sekali roadmap kali ini
+    }
+}
